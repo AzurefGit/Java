fa78e3c658ff21897b8c7f09c80ebfab57714a95
AzurefGit <losowyemail@email.em>
2024-10-24 15:53:02 +0200

    a

--- zaj2/src/zad1F/Main.java
+++ zaj2/src/zad1F/Main.java
@@ -1,9 +1,9 @@
 package zad1F;
 
 public class Main {
-    public static void Main(String[] args){
-        zad1F.Point p1 = new Point(1,2);
-        zad1F.Point p2 = new Point(3,4);
+    public static void main(String[] args) {
+        zad1F.Point p1 = new Point(1,0);
+        zad1F.Point p2 = new Point(3,0);
         double dys = p1.distance(p2);
         System.out.println(dys);
     }
